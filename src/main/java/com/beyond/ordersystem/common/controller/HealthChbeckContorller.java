33e6c64885096c7c9306b91ae681e85eafdef74f
wooyoungking <ggj0228@naver.com>
2025-09-01 10:16:05 +0900

    f

---
+++ src/main/java/com/beyond/ordersystem/common/controller/HealthChbeckContorller.java
@@ -0,0 +1,24 @@
+package com.beyond.ordersystem.common.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.util.Map;
+
+@RestController
+public class HealthChbeckContorller {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> probe() {
+        // 여기서는 절대 DB 호출하지 말 것 (빠른 200 응답 필요)
+        return ResponseEntity.ok(
+                Map.of(
+                        "status", "UP",
+                        "time", OffsetDateTime.now().toString()
+                )
+        );
+    }
+}
+
